a796b9bb8dd1988df7957a2c18c523a5283aa613
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-03-05 09:48:38 +0100

    #3 data img en naam producten ingeladen

--- server.js
+++ server.js
@@ -40,7 +40,22 @@ app.set('views', './views')
 app.get('/', async function (request, response) {
    // Render index.liquid uit de Views map
    // Geef hier eventueel data aan mee
-   response.render('index.liquid')
+     const params = {
+    // Sorteer op naam
+    // 'sort': 'name',
+ 
+    // Geef aan welke data je per persoon wil terugkrijgen
+    'fields': 'name,image',
+ 
+    // Combineer meerdere filters
+ 
+  }
+
+ const productResponse = await fetch('https://fdnd-agency.directus.app/items/milledoni_products/?' + new URLSearchParams(params))
+ 
+const productResponseJSON = await productResponse.json()
+  console.log(productResponseJSON.data)
+   response.render('index.liquid',{products: productResponseJSON.data}) 
 })
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
