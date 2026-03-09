8039aee3b8b786a174b2b61f97c2f46f0c40fe22
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-03-09 15:07:12 +0100

    #6 404 pagina

--- server.js
+++ server.js
@@ -57,6 +57,15 @@ const productResponseJSON = await productResponse.json()
   console.log(productResponseJSON.data)
    response.render('index.liquid',{products: productResponseJSON.data}) 
 })
+ 
+
+
+app.use((req, res, next)=>{
+
+  res.status(404).render('404.liquid')
+})
+
+
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
